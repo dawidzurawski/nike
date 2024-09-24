627845493a2a5b482d44d7801e5c6e178d63fc34
dawidzurawski <dawidzurawskidrz@gmail.com>
2024-09-24 22:21:25 +0100

    Add app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import Hero from './sections/Hero'
-import PopularProducts from './sections/PopularProducts'
-import SuperQuality from './sections/SuperQuality'
-import Services from './sections/Services'
-import SpecialOffers from './sections/SpecialOffers'
-import CustomerReviews from './sections/CustomerReviews'
-import Subscribe from './sections/Subscribe'
-import Footer from './sections/Footer'
+import {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffers,
+  CustomerReviews,
+  Subscribe,
+  Footer
+} from './sections'
 
 const App = () => {
   return (
     <main className="relative">
-      Nav
+      {/* Nav */}
       <section className="xl:padding-1 wide:padding-r padding-b">
         <Hero />
       </section>
